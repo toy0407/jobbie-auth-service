947feba8aa0a4609812570d557c4148e91d4c9f1
Suvro Bose <bose.suvro.com>
2025-03-25 09:30:56 +0530

    temp

--- src/handlers/signup.ts
+++ src/handlers/signup.ts
@@ -1,57 +1,8 @@
 import { APIGatewayProxyEvent, Context } from "aws-lambda";
 import { lambdaHandler } from "@toy0407/jobbie-lib/dist/utils/lambdaWrapper.util";
-import { getPrismaClient } from "@toy0407/jobbie-lib/dist/db/client";
-import { faker } from "@faker-js/faker";
+import { DbClient } from "@toy0407/jobbie-lib/dist/db/client";
+import { logger } from "@toy0407/jobbie-lib/dist/utils/logger.util";
 
-async function signUp(event: APIGatewayProxyEvent, context: Context) {
-  //   createBulkUsers(100000);
-  console.log(process.env.DATABASE_URL);
-  try {
-    await getPrismaClient().user.findMany({
-      where: {
-        email: "asdf@example.com",
-      },
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-const BATCH_SIZE = 500; // Optimal for most databases
-const CONCURRENCY = 2; // Parallel batches
-
-async function createBulkUsers(totalUsers: number) {
-  let inserted = 0;
-
-  // Generate mock data
-  const createUserData = async () => ({
-    email: faker.internet.email().toLowerCase(),
-    name: faker.person.fullName(),
-  });
-
-  // Process in parallel batches
-  while (inserted < totalUsers) {
-    const batchPromises = Array.from({ length: CONCURRENCY }, async () => {
-      const users = await Promise.all(
-        Array.from({ length: BATCH_SIZE }, createUserData)
-      );
-
-      await getPrismaClient().$transaction([
-        getPrismaClient().user.createMany({
-          data: users,
-          skipDuplicates: true,
-        }),
-      ]);
-    });
-
-    try {
-      await Promise.all(batchPromises);
-      inserted += BATCH_SIZE * CONCURRENCY;
-      console.log(`Inserted ${inserted}/${totalUsers} users`);
-    } catch (error) {
-      console.error("Batch failed:", error);
-    }
-  }
-}
+async function signUp(event: APIGatewayProxyEvent, context: Context) {}
 
 export const handler = lambdaHandler(signUp);
